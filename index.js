b3ac91c3c49c6585b268a1371c3e946e35ef52f4
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-08-17 07:32:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <=400){
+    return 'This one is on me!';
+  }
+else if 
+  (ride >2000 && ride <=2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if
+  (ride >=2500){
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const destination = city === "NYC"? "Ok, sounds good." : "No go.";
+  return destination;  
 }
+ternaryCheckCity("NYC")
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+
+      case "not as generous":
+        return "Thank you.";
+
+        default:
+          return "Bye."
+  }
+  
 }
